2f0f0a4e0b9c5c15519919885ad0d7fb24441199
pardax <get_ant@naver.com>
2023-02-27 12:45:32 +0900

    0227

--- SBSProject/Idle.cpp
+++ SBSProject/Idle.cpp
@@ -18,9 +18,13 @@ private:
 
 	static int g_nScreenIndex;
 	static HANDLE g_hScreen[2];
-	int g_numofFPS;
-	clock_t CurTime, OldTime;
 	char* FPSTextInfo;
+	clock_t CurTime, OldTime;
+
+	int g_numOfFrame;//누적 프레임
+	int g_numOfFPS;//분당 프레임
+
+
 
 public:
     Idle() {
@@ -36,14 +40,14 @@ public:
 		ifstream file_2(path_2);
 		if (file_2.is_open()) {
 			while (getline(file_2, temp)) {
-				idle_1.push_back(temp);
+				idle_2.push_back(temp);
 			}
 			file_2.close();
 		}
 		ifstream file_3(path_1);
 		if (file_3.is_open()) {
 			while (getline(file_3, temp)) {
-				idle_1.push_back(temp);
+				idle_3.push_back(temp);
 			}
 			file_3.close();
 		}
@@ -68,11 +72,37 @@ public:
 		FillConsoleOutputCharacter(g_hScreen[g_nScreenIndex], ' ', 80 * 40, coor, &dw);
 	}
 
+	void ScreenFilp() {
+		SetConsoleActiveScreenBuffer(g_hScreen[g_nScreenIndex]);
+		g_nScreenIndex = !g_nScreenIndex;
+	}
+
 	void ScreenPrint(char* string) {
 		DWORD dw;
 		COORD cursorPos = { 0, 0 };
 		SetConsoleCursorPosition(g_hScreen[g_nScreenIndex], cursorPos);
 		WriteFile(g_hScreen[g_nScreenIndex], string, strlen(string), &dw, NULL);
+	}
+
+	void Render() {
+		ScreenClear();
+		if (CurTime - OldTime >= 1000)
+		{
+			OldTime = CurTime;
+			g_numOfFPS = g_numOfFrame;
+			g_numOfFrame = 0;
+		}
+		/*
+		sprintf_s(FPSTextInfo, , "FPS : %d , deltaTime : %d\n", g_numOfFPS, );
+		ScreenPrint(0, 0, FPSTextInfo);
+		g_numOfFrame++;
+		*/
+
+		ScreenFilp();
+
+	}
+
+	void TempPrint() {
 
 	}
 
@@ -81,6 +111,7 @@ public:
 		{
 			cout << idle_1[i] << '\n';
 		}
+		//ScreenClear();
 	}
 
 };
